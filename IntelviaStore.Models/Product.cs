9864931e8853a736fb9cdbcb9b2b7e4fe468c548
Iliass Raihani <i.raihani@outlook.com>
2021-04-17 10:43:07 +0000

    redesign product models

--- IntelviaStore.Models/Product.cs
+++ IntelviaStore.Models/Product.cs
@@ -2,22 +2,30 @@
 
 namespace IntelviaStore.Models
 {
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string CoverPhotoPath { get; set; }
+        public Product Product { get; set; }
+    }
+
     public class Product
     {
         public int ProductID { get; set; }
         public string ProductName { get; set; }
         public string ProductDescription { get; set; }
-        public Category Category { get; set; }
         public double UnitPrice { get; set; }
         public DateTime UpdatedOn { get; set; }
+        public Galery Galery { get; set; }
+        public int IncludeInCategoryID { get; set; }
         public int CreatedByUserID { get; set; }
-        public string PhotoPath { get; set; }
     }
 
-    public class Category
+    public class Galery
     {
-        public int CategoryID { get; set; }
-        public string CategoryName { get; set; }
+        public int GaleryID { get; set; }
+        public int OfTheProductID { get; set; }
         public string PhotoPath { get; set; }
     }
 }
--- IntelviaStore.Web/Pages/ProductListBase.cs
+++ IntelviaStore.Web/Pages/ProductListBase.cs
@@ -9,6 +9,7 @@ namespace IntelviaStore.Web.Pages
     public class ProductListBase : ComponentBase
     {
         public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
 
         protected override Task OnInitializedAsync()
         {
@@ -24,8 +25,7 @@ namespace IntelviaStore.Web.Pages
                 ProductName = "Produit 01",
                 UnitPrice = 1.99,
                 UpdatedOn = new DateTime(1980, 10, 5),
-                Category = new Category { CategoryID = 1, CategoryName = "Montre" },
-                PhotoPath = "images/watch01.png"
+                Galery = new Galery { GaleryID = 1, PhotoPath = "images / watch01.png" }
             };
 
             Product p2 = new Product
@@ -34,8 +34,7 @@ namespace IntelviaStore.Web.Pages
                 ProductName = "Produit 02",
                 UnitPrice = 1.99,
                 UpdatedOn = new DateTime(1980, 10, 5),
-                Category = new Category { CategoryID = 2, CategoryName = "Livre" },
-                PhotoPath = "images/book01.png"
+                Galery = new Galery { GaleryID = 2, PhotoPath = "images/book01.png" }
             };
 
             Product p3 = new Product
@@ -44,8 +43,7 @@ namespace IntelviaStore.Web.Pages
                 ProductName = "Produit 03",
                 UnitPrice = 1.99,
                 UpdatedOn = new DateTime(1980, 10, 5),
-                Category = new Category { CategoryID = 1, CategoryName = "Montre" },
-                PhotoPath = "images/watch02.png"
+                Galery = new Galery { GaleryID = 2, PhotoPath = "images/watch02.png" } 
             };
 
             Product p4 = new Product
@@ -54,8 +52,7 @@ namespace IntelviaStore.Web.Pages
                 ProductName = "Produit 04",
                 UnitPrice = 1.99,
                 UpdatedOn = new DateTime(1980, 10, 5),
-                Category = new Category { CategoryID = 2, CategoryName = "Livre" },
-                PhotoPath = "images/book02.png"
+                Galery = new Galery { GaleryID = 2, PhotoPath = "images/book02.png" }
             };
 
             Products = new List<Product> { p1, p2, p3, p4 };
