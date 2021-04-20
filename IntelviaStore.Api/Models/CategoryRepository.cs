5b78d176a9731e334552337f87cafbf2a86201ca
Iliass Raihani <i.raihani@outlook.com>
2021-04-20 01:40:35 +0000

    create category api model

---
+++ IntelviaStore.Api/Models/CategoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using IntelviaStore.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IntelviaStore.Api.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext appDbContext;
+
+        public CategoryRepository(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public Category GetCategory(int departmentId)
+        {
+            return appDbContext.Categories
+                .FirstOrDefault(d => d.CategoryID == departmentId);
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            return appDbContext.Categories;
+        }
+    }
+}
---
+++ IntelviaStore.Api/Models/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using IntelviaStore.Models;
+using System.Collections.Generic;
+
+namespace IntelviaStore.Api.Models
+{
+    interface ICategoryRepository
+    {
+        IEnumerable<Category> GetCategories();
+        Category GetCategory(int categoryId);
+    }
+}
