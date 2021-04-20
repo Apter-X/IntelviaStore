baba1fb654edbf1a86cb793028a1037b901be286
Iliass Raihani <i.raihani@outlook.com>
2021-04-20 01:40:13 +0000

    create product api model

---
+++ IntelviaStore.Api/Models/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using IntelviaStore.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IntelviaStore.Api.Models
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProduct(int productId);
+        Task<Product> AddProduct(Product product);
+        Task<Product> UpdateProduct(Product product);
+        void DeleteProduct(int productId);
+    }
+}
---
+++ IntelviaStore.Api/Models/ProductRepository.cs
@@ -0,0 +1,67 @@
+﻿using IntelviaStore.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IntelviaStore.Api.Models
+{
+    public class EmployeeRepository : IProductRepository
+    {
+        private readonly AppDbContext appDbContext;
+
+        public EmployeeRepository(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await appDbContext.Products.ToListAsync();
+        }
+
+        public async Task<Product> GetProduct(int productId)
+        {
+            return await appDbContext.Products
+                .FirstOrDefaultAsync(e => e.ProductID == productId);
+        }
+
+        public async Task<Product> AddProduct(Product product)
+        {
+            var result = await appDbContext.Products.AddAsync(product);
+            await appDbContext.SaveChangesAsync();
+            return result.Entity;
+        }
+
+        public async Task<Product> UpdateProduct(Product product)
+        {
+            var result = await appDbContext.Products
+                .FirstOrDefaultAsync(e => e.ProductID == product.ProductID);
+
+            if (result != null)
+            {
+                result.UpdatedOn = product.UpdatedOn;
+                result.ProductName = product.ProductName;
+                result.ProductDescription = product.ProductDescription;
+                result.UnitPrice = product.UnitPrice;
+                result.IncludeInCategoryID = product.IncludeInCategoryID;
+
+                await appDbContext.SaveChangesAsync();
+
+                return result;
+            }
+
+            return null;
+        }
+
+        public async void DeleteProduct(int productId)
+        {
+            var result = await appDbContext.Products
+                .FirstOrDefaultAsync(e => e.ProductID == productId);
+            if (result != null)
+            {
+                appDbContext.Products.Remove(result);
+                await appDbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
