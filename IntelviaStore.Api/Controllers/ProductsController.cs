1cdb5762994849c43611502350ca35cc986b6ed1
Iliass Raihani <i.raihani@outlook.com>
2021-04-26 23:25:28 +0000

    Create ProductsController.cs

---
+++ IntelviaStore.Api/Controllers/ProductsController.cs
@@ -0,0 +1,73 @@
+﻿using IntelviaStore.Api.Models;
+using IntelviaStore.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace IntelviaStore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductsController(IProductRepository employeeRepository)
+        {
+            this.productRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetProducts()
+        {
+            try
+            {
+                return Ok(await productRepository.GetProducts());
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from the database");
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            try
+            {
+                var result = await productRepository.GetProduct(id);
+
+                if (result == null) return NotFound();
+
+                return result;
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from the database");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> CreateProduct(Product product)
+        {
+            try
+            {
+                if (product == null)
+                    return BadRequest();
+
+                var createdProduct = await productRepository.AddProduct(product);
+
+                return CreatedAtAction(nameof(GetProduct),
+                    new { id = createdProduct.ProductID }, createdProduct);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error creating new employee record");
+            }
+        }
+    }
+}
